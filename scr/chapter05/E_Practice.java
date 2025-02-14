decc937efc663a18fe5935f147838742bb99344c
iasos2 <ykkminji@gmail.com>
2025-02-14 16:24:38 +0900

    Create E_Practice.java

---
+++ scr/chapter05/E_Practice.java
@@ -0,0 +1,18 @@
+package chapter05;
+
+	// === 인스턴스 변수 ===
+    // 선수 이름(name -  문자열)
+    // 타율(battingAverage - 실수)
+    // 홈런 수(homeRuns - 정수)
+	
+	// === 정적(클래스) 변수 ===
+    // : 생성된 선수의 총 인원(playerCount - 정수)
+    // >> 모든 인스턴스에서 공유
+	
+
+
+public class E_Practice {
+	public static void main(String[] args) {
+		
+	}
+}
