43c97fc4214b7ef8224d2cf48d12ad870561c3fd
iasos2 <ykkminji@gmail.com>
2025-02-27 16:20:46 +0900

    Create ReservationServiceImpl.java

---
+++ scr/user_reservation/service /implement /ReservationServiceImpl.java
@@ -0,0 +1,25 @@
+package user_reservation.service.implement;
+
+import java.util.List;
+
+import user_reservation.entity.Reservation;
+import user_reservation.service.ReservationService;
+
+public class ReservationServiceImpl implements ReservationService{
+
+	@Override
+	public void createReservation(String userId) {
+		
+	}
+
+	@Override
+	public List<Reservation> getReservationsByUserId(String userId) {
+		return null;
+	}
+
+	@Override
+	public void cancelReservation(String reservationId) {
+		
+	}
+
+}
