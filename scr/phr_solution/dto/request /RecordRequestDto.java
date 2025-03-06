24636745950c8ec0552821178992849884916cd9
iasos2 <ykkminji@gmail.com>
2025-03-06 11:58:56 +0900

    Update RecordRequestDto.java

--- scr/phr_solution/dto/request /RecordRequestDto.java
+++ scr/phr_solution/dto/request /RecordRequestDto.java
@@ -1,5 +1,13 @@
 package phr_solution.dto.request;
 
-public class RecordRequestDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class RecordRequestDto {
+	private Long patientId;
+	private String dateOfVisit;
+	private String diagnosis;
+	private String treatment;
 }
