0c9a8a1715e103b0c71d9bde904a05df2e673301
iasos2 <ykkminji@gmail.com>
2025-03-06 11:58:39 +0900

    Update PatientRequestDto.java

--- scr/phr_solution/dto/request /PatientRequestDto.java
+++ scr/phr_solution/dto/request /PatientRequestDto.java
@@ -1,5 +1,12 @@
 package phr_solution.dto.request;
 
-public class PatientRequestDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class PatientRequestDto {
+	private String name;
+	private int age;
+	private String gender;
 }
